00dc070788b3f3d9bd432dd0b5061b93f2e7b3db
mgunduz1 <mgunduz114118@gmail.com>
2021-06-03 18:31:02 +0300

    add group pages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,7 @@ class GroupsController < ApplicationController
 
   # GET /groups/1 or /groups/1.json
   def show
+    @transactions = Transaction.where(group_id: @group.id)
   end
 
   # GET /groups/new
